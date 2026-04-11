4c88429dbbe48760915f6c353367a3eb9a98af80
fgloris <stepthenfriedman@gmail.com>
2026-04-11 13:17:29 +0800

    modified val env settings

--- agent_ppo/feature/preprocessor.py
+++ agent_ppo/feature/preprocessor.py
@@ -240,7 +240,9 @@ class Preprocessor:
         # 进度特征
         step_norm = _norm(self.step_no, self.max_step)
         progress_treasure_collect = _norm(int(hero.get("treasure_collected_count", 0)), 10)
-        time_before_second_mounster = _norm(max(0, env_info.get("monster_interval", 300) - self.step_no), self.max_step)
+        monster_interval = env_info.get("monster_interval", 300)
+        assert monster_interval > 0, f"monster insterval < 0! value:{monster_interval}"
+        time_before_second_mounster = _norm(max(0, monster_interval - self.step_no), self.max_step)
         has_monster_speedup = 0.0 if env_info.get("monster_speed", 0) <= 1 else 1.0
         progress_feat = np.array([step_norm, progress_treasure_collect, time_before_second_mounster, has_monster_speedup], dtype=np.float32)
 
